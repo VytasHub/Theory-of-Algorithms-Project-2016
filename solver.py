d81374bcf785c9b644e3e39359a70ebde79bf13d
VytasHub <vytasvaiciulisgti@gmail.com>
2016-03-23 22:48:31 +0000

    wordList
    wordList Read in

--- solver.py
+++ solver.py
@@ -6,8 +6,17 @@
 import random as rn
 
 # This preprocessing function loads the words list file into a Python list.
-def preprocessing(filename="wordslist.txt"):
-  return []
+def preprocessing(fname):
+	with open(fname) as f:
+		content = f.readlines()
+	return [content]
+  
+  
+preprocessing("wordlist.txt")
+  
+print("Hi")
+
+print(preprocessing("wordlist.txt"))
   
 # This is the function that actually checks the random letters for words.
 def check(letters):
@@ -17,3 +26,6 @@ def check(letters):
 
 # This function is just a wrapper that shows how my script works.
 # It does the preprocessing, then creates a random list of letters, and finally runs the solver.
+
+
+#Add words from txt to to a list , than order them by lenght 
