9aef18aaccdf4446277e9401bce0385343133073
VytasHub <vytasvaiciulisgti@gmail.com>
2016-03-26 18:25:49 +0000

    Updated
    Version_1

--- Versions/Version_1.py
+++ Versions/Version_1.py
@@ -2,59 +2,50 @@
 # G00304571
 # Date
 
+from itertools import permutations
+import random
+import string
 
-dictionary = [] #Creates dictionary list
-angramWords = [] #Creats angramWords list
 
+result = []
 
 with open("wordlist.txt") as f: # Opens up wordlist.txt and saves in to the dictionary list
-	dictionary = [line.strip() for line in f]# Brackets , its list comprahension , line.strip() strips of unvanted characters as /n if dont do this words wount match as they all contain /n 
+	dictionary = {line.strip() for line in f}#line.strip() strips of unvanted characters as /n if dont do this words wount match as they all contain /n 
+f.close()
 print("Dictionary loaded...")	
 	
 
 
  
-def anagrams(letters): # Finds all angrams of a given word
-  if len(letters) <=1: # Wount look for angrams if word is 0 or 1
-    return letters
-  else:
-    anagramlist = []
-    for anagram in anagrams(letters[1:]): # Finds all angrams by switching char around 1 at time
-      for i in range(len(letters)):
-        anagramlist.append(anagram[:i] + letters[0:1] + anagram[i:])
-    return anagramlist
-print("Angrams Found...")	
 
-word = "auctieond" #Word used to find angrams
-#auctioned
+word = "auctieond"
 
-
-angramWords = anagrams(word) #Populates globaly decleared list with all the anagrams
+#http://stackoverflow.com/questions/23159200/how-to-get-every-single-permutation-of-a-string
+allPermu = ([''.join(p) for i in range(1, len(word)+1) for p in permutations(word, i)])
+#perms = [''.join(p) for p in permutations(word)]
 
 
 print("Checking Angrams...")
 def checker():						#Compares all Angrams agianst the dictionary
-	for angram in angramWords:
+	for angram in allPermu:
 		if angram in dictionary:
 			if len (angram) > 8:    #Tryes to find longest angram first of lenght 9 and works its way down
-				print(angram)
+				result.append(angram)
 			elif len (angram) > 7:
-				print(angram)
+				result.append(angram)
 			elif len (angram) > 6:
-				print(angram)
-			elif len (angram) > 5:
-				print(angram)
-			elif len (angram) > 4:
-				print(angram)
-			elif len (angram) > 3:
-				print(angram)
-			elif len (angram) > 2:
-				print(angram)
-				
-				
-				
+				result.append(angram)
+			elif len (angram) > 5:	
+				result.append(angram)
+			elif len (angram) > 4:	
+				result.append(angram)
+			elif len (angram) > 3:	
+				result.append(angram)
+			elif len (angram) > 2:	
+				result.append(angram)
 checker()
 
+print(result)
 # Other way checking Angrams agains the Dictionary
 #for anagram in angramWords:
 	#print("Outer " + anagram)
--- Versions/Version_3.py
+++ Versions/Version_3.py
@@ -3,13 +3,8 @@
 # Date
 
 
-import time
-import hashlib
-
-
 
 mapDctionary = dict()
-mapFilter = dict()
 mapFilter = dict()
 asciiValueInt = 0
 asciiValueConcat = 0
@@ -17,49 +12,40 @@ finalResult = []
 
 
 print("Preprocessing...")
-with open("bigDictionary.txt") as f:
+with open("wordlist.txt") as f:
+	#dictionary = f.readlines()
 	dictionary = [line.strip() for line in f]# Brackets , its list comprahension
 f.close()
 
 
 
 for word in dictionary:
-	ascii = str(sum(ord(ch) for ch in word))
-	hash_object = hashlib.md5(ascii.encode())
-	mapDctionary.update({word:hash_object.hexdigest()})
-	
+	mapDctionary.update({word:sum(ord(ch) for ch in word)})
 
 	
 def proccesWord():
 	word = "auctinoed"
-	ascii = str(sum(ord(ch) for ch in word))
-	hash_object = hashlib.md5(ascii.encode())
-	hash_object.hexdigest()
+	asciiValueInt = (sum(ord(ch) for ch in word))
 	sortword = sorted(word)
 	asciiConcat = [str(ord(ch)) for ch in sortword]
 	asciiValueConcat = "".join(asciiConcat)
 	
-	return hash_object.hexdigest(),int(asciiValueConcat)
+	return asciiValueInt,int(asciiValueConcat)
 	
 asciiValueInt, asciiValueConcat = proccesWord()
 
 
-
 def	checAnagrams():	
-	for word,count in mapDctionary.items():
-		
+	for word, count in mapDctionary.items():
 		if count == asciiValueInt:
-			
 			sortword = sorted(word)
 			ascii = [str(ord(ch)) for ch in sortword] 
 			asciiJoined = "".join(ascii)
 			mapFilter.update({word:int(asciiJoined)})
 	return mapFilter
-
-
+	
 	
 mapFilter = checAnagrams()
-
 	
 
 def	checAnagrams():	
@@ -68,10 +54,6 @@ def	checAnagrams():
 			print(word)
 	
 checAnagrams()
-
-
-
-
 
 
 
